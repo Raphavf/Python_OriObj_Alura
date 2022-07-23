1ce9cd558a2a4b4364f6a13783100762ad0dd015
Raphavf <47055553+Raphavf@users.noreply.github.com>
2022-07-23 15:37:40 -0300

    Primeira Classe

--- conta.py
+++ conta.py
@@ -2,5 +2,10 @@
 
 class Conta:
 
-    pass
+    def __init__(self,numero , titular, saldo, limite):
+        print("Construindo Objeto ... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
 
